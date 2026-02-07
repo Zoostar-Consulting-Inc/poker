12da58f28bb9c969e376eb25317a5fb3e422458f
zoostar <devops@zoostar.net>
2026-02-07 14:43:25 -0500

    Feature/3.3.0 (#19)
    * Force build
    
    * Add JUnit coverage for failing SonarScan
    
    * Fix Sonar issues
    
    * Fix Sonar issues
    
    * Fix Sonar issues
    
    * Add Sonar Quality Gate Badge
    
    * Add Sonar Quality Gate Badge
    
    * Add Sonar Quality Gate Badge
    
    * Remove OAuth2 temporarily for ordering SSL Cert.

--- poker-api/src/main/java/com/zoostarinc/poker/web/controller/SwaggerController.java
+++ poker-api/src/main/java/com/zoostarinc/poker/web/controller/SwaggerController.java
@@ -7,9 +7,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,18 +27,12 @@ public class SwaggerController implements ApplicationContextAware {
 	}
 	
 	@GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
-	public String getSwaggerUI(@AuthenticationPrincipal DefaultOidcUser user, Model model, HttpSession session) {
+	public String getSwaggerUI(Model model, HttpSession session) {
 		String page = "redirect:swagger-ui/index.html";
 		log.debug("Loading greeting in env: {}",
 				Arrays.toString(applicationContext.getEnvironment().getActiveProfiles()));
 		log.debug("Session ID: {}", session.getId());
 
-		var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		log.debug("Logged in Principal: {}", principal.toString());
-		var name = user == null ? "Guest" : user.getGivenName();
-		log.info("Hello {}!", name);
-
-		model.addAttribute("name", name);
 		model.addAttribute("currentTime", OffsetDateTime.now().format(Utils.ISO_DATE_TIME_FORMAT_UPTO_SECONDS));
 
 		return page;
