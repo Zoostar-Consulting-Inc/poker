05884f3e426212b7bcbee101b85733c6e0c39499
zoostar <devops@zoostar.net>
2026-02-10 12:02:07 -0500

    Revert "Remove OAuth2 temporarily for ordering SSL Cert."
    This reverts commit 92b4d5479631e6ea740cb125e5b257037e22113e.

--- poker-api/src/test/java/com/zoostarinc/poker/AbstractCommonTest.java
+++ poker-api/src/test/java/com/zoostarinc/poker/AbstractCommonTest.java
@@ -1,11 +1,23 @@
 package com.zoostarinc.poker;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -25,15 +37,26 @@ public abstract class AbstractCommonTest {
 	}
 
 	protected MockHttpServletResponse getBody(String url) throws Exception {
-		return endpoint.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+		return endpoint.perform(get(url).contentType(MediaType.APPLICATION_JSON).with(oidcLogin().oidcUser(oidcUser())))
 				.andReturn().getResponse();
 	}
 
 	protected <T> MockHttpServletResponse postJsonRequest(String url, T request) throws Exception {
 		return endpoint
 				.perform(post(url).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
-						.content(om.writeValueAsString(request)))
+						.content(om.writeValueAsString(request)).with(oidcLogin().oidcUser(oidcUser())))
 				.andReturn().getResponse();
+	}
+
+	protected OidcUser oidcUser() {
+		Map<String, Object> claims = new HashMap<>();
+		claims.put(StandardClaimNames.SUB, "testuser");
+		claims.put(StandardClaimNames.GIVEN_NAME, "junit");
+		claims.put(StandardClaimNames.EMAIL, "test@example.com");
+		OidcIdToken idToken = new OidcIdToken("tokenValue", Instant.now(), Instant.now().plusSeconds(3600), claims);
+		OidcUserInfo userInfo = new OidcUserInfo(claims);
+		return new DefaultOidcUser(Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")), idToken,
+				userInfo);
 	}
 
 }
